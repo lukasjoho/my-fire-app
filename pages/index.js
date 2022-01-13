996b809b54f90e1d4e7251f0ee4b8f83232c5b0b
lukasjoho <social@lukashoppe.com>
2022-01-13 19:36:32 +0100

    Create login logout functionality

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,50 @@
+import { auth, firestore } from '../lib/firebase';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 export default function Home() {
-	return <div>My Fire App</div>;
+	const [user] = useAuthState(auth);
+	const SignInButton = () => {
+		const provider = new GoogleAuthProvider();
+		const signInWithGoogle = async () => {
+			await signInWithPopup(auth, provider).then((result) => {
+				const { user } = result;
+				console.log(user);
+				return user;
+			});
+		};
+		return (
+			<button className='login' onClick={signInWithGoogle}>
+				Sign In With Google
+			</button>
+		);
+	};
+
+	const LogOutButton = () => {
+		const logOut = async () => {
+			await signOut(auth);
+		};
+		return (
+			<button className='logout' onClick={logOut}>
+				Log Out
+			</button>
+		);
+	};
+
+	return (
+		<div>
+			{user ? (
+				<div>
+					You are logged in!
+					<LogOutButton />
+					<div>FEED</div>
+				</div>
+			) : (
+				<div>
+					Please login!
+					<SignInButton />
+				</div>
+			)}
+		</div>
+	);
 }
